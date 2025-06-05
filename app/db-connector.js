dd19d85ab487001cd6daa8112c243312ce291861
bspier95 <spierb@oregonstate.edu>
2025-06-05 08:02:20 -0700

    update to db-connectors

--- app/db-connector.js
+++ app/db-connector.js
@@ -1,11 +1,30 @@
 const mysql = require('mysql');
+const url = require('url');
 
-const pool = mysql.createPool({
-  connectionLimit: 10,
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'your_local_user',
-  password: process.env.DB_PASSWORD || 'your_local_password',
-  database: process.env.DB_NAME || 'your_local_db_name',
-});
+let config;
 
+if (process.env.DATABASE_URL) {
+  const dbUrl = url.parse(process.env.DATABASE_URL);
+  const [user, password] = dbUrl.auth.split(':');
+
+  config = {
+    connectionLimit: 10,
+    host: dbUrl.hostname,
+    user,
+    password,
+    database: dbUrl.pathname.replace('/', ''), // remove leading slash
+    port: dbUrl.port || 3306
+  };
+} else {
+  // fallback for local development
+  config = {
+    connectionLimit: 10,
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'your_local_user',
+    password: process.env.DB_PASSWORD || 'your_local_password',
+    database: process.env.DB_NAME || 'your_local_db_name',
+  };
+}
+
+const pool = mysql.createPool(config);
 module.exports.pool = pool;
